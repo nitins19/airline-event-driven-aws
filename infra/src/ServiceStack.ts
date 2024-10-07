7f6e0b07e1acedfe4ada7a6f0697fe65cf87651c
Nitin Saxena <nitinsaxena913@gmail.com>
2024-10-07 20:21:21 +0530

    correcting

--- infra/src/ServiceStack.ts
+++ infra/src/ServiceStack.ts
@@ -17,7 +17,7 @@ export default class ServiceStack extends Stack {
 
         const serviceLambda = new NodejsFunction(this, 'ServiceLambda', {
             runtime: Runtime.NODEJS_18_X,
-            entry: path.join(__dirname, "../service/src/lambda.js"),
+            entry: path.join(__dirname, "../service/src/lambda.ts"),
             handler: "index.handler",
             timeout: Duration.seconds(10),
             memorySize: 250,
