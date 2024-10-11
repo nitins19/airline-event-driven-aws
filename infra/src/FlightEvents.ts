baf30118fe20f4491a5c949fe2a43befbb297a8e
Nitin Saxena <nitinsaxena913@gmail.com>
2024-10-11 17:15:50 +0530

    adding log group for pipe

--- infra/src/FlightEvents.ts
+++ infra/src/FlightEvents.ts
@@ -19,7 +19,7 @@ export default class FlightEvents extends Construct {
             eventBusName: "FlightOrderEventsBus",
         });
 
-        const eventBusLogGroup = new LogGroup(this, "EventBusRule", {
+        const eventBusLogGroup = new LogGroup(this, "EventBusLogGroup", {
             retention: RetentionDays.ONE_DAY,
             removalPolicy: RemovalPolicy.DESTROY,
         });
@@ -38,8 +38,14 @@ export default class FlightEvents extends Construct {
             assumedBy: new ServicePrincipal("pipes.amazonaws.com"),
         });
 
+        const pipeLogGroup = new LogGroup(this, "EventBridgePipeLogGroup", {
+            retention: RetentionDays.ONE_DAY,
+            removalPolicy: RemovalPolicy.DESTROY,
+          });
+
         eventBus.grantPutEventsTo(pipeRole);
         props.flightOrderTable.grantStreamRead(pipeRole);
+        pipeLogGroup.grantWrite(pipeRole);
 
         const pipe = new CfnPipe(this, 'DynamoDBtoEventBusPipe', {
             name: 'EventOrderPipe',
