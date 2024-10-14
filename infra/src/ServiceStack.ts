7f19e47537fb06854c67e11c16f93a5cd5e7787a
Nitin Saxena <nitinsaxena913@gmail.com>
2024-10-14 21:08:44 +0530

    giving lambda permission to putEvents

--- infra/src/ServiceStack.ts
+++ infra/src/ServiceStack.ts
@@ -141,6 +141,8 @@ export default class ServiceStack extends Stack {
             },
         });
 
+        eventBus.grantPutEventsTo(processOrderLambda);
+
         const processOrderTask = new LambdaInvoke(this, "ProcessOrderStep", {
             lambdaFunction: processOrderLambda,
             payload: TaskInput.fromJsonPathAt("$"),
